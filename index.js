13df7a0fade6c95d4af7e02df32aff3c78dc72d8
jaimedavid01 <jaimedavid01@yahoo.com>
2021-01-13 16:33:10 -0500

    Done

--- index.js
+++ index.js
@@ -2,5 +2,48 @@ document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
 
-  // ADD CODE HERE!
+  
+  const moveList = document.querySelector("#moves-container")
+  const moveButton = document.querySelector("#move-button")
+
+
+
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+      const moveLi = document.createElement("li")
+      moveLi.textContent = "left"
+      moveList.append(moveLi)
+    }
+    else if (event.key === "ArrowRight") {
+      const moveLi = document.createElement("li")
+      moveLi.textContent = "right"
+      moveList.append(moveLi)
+    }
+    else if (event.key === "ArrowDown") {
+      const moveLi = document.createElement("li")
+      moveLi.textContent = "down"
+      moveList.append(moveLi)
+    }
+    else if (event.key === "ArrowUp") {
+      const moveLi = document.createElement("li")
+      moveLi.textContent = "up"
+      moveList.append(moveLi)
+    }
+  })
+  
+  moveButton.addEventListener("click", function(event) {
+    const nextMove = moveList.querySelectorAll("li")
+    nextMove.forEach((differentMove, index) => { 
+      setTimeout(() => { 
+        move(differentMove.textContent)
+        differentMove.remove()
+        }, 500*(index +1))
+    })
+  })
+
+  moveList.addEventListener("click", function(event){
+    if (event.target.matches("li")){
+      event.target.remove()
+    }
+  })
 });
